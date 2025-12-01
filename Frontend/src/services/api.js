8abdc9e04bdfb4179a7cd0968ea2d383b8905068
Ashoknand <108652450+Ashoknand@users.noreply.github.com>
2025-12-01 11:39:27 +0530

    Update api.js

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api" || "https://voting-system-faoa.onrender.com";
+  import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api" || "https://voting-system-faoa.onrender.com/api";
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
